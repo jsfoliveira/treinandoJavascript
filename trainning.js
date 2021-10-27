c5941b62667ae322f8ee78dc54d6b1638adeeb97
Juliana Oliveira <jsfoliveira89@gmail.com>
2021-10-27 16:19:39 -0300

    criei outra calculadora usando javaScript

--- trainning.js
+++ trainning.js
@@ -21,3 +21,31 @@ function calcular() {
     window.document.getElementById("resultado").innerHTML = eval(resultado);
   }
 }
+//aqui começa o código da segunda calculadora
+function calculation() {
+  var a = parseInt(window.document.querySelector("input#value1").value);
+  //parseInt converte  string para números inteiros
+  var b = parseInt(window.document.querySelector("input#value2").value);
+  var selectOperation = window.document.querySelector(
+    'input[name="operation"]:checked'
+  ).value;
+  //eu só quero pegar o "name=operation" que esteja clicado
+  var nameOperation, calculo;
+  if (selectOperation == "adicao") {
+    nameOperation = "Adição";
+    calculo = a + b;
+  }
+  if (selectOperation == "subtracao") {
+    nameOperation = "Subtração";
+    calculo = a - b;
+  }
+  if (selectOperation == "multiplicacao") {
+    nameOperation = "Multiplicação";
+    calculo = a * b;
+  }
+  if (selectOperation == "divisao") {
+    nameOperation = "Divisão";
+    calculo = a / b;
+  }
+  alert(nameOperation + ":" + calculo);
+}
